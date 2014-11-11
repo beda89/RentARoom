4df2e6b87a9bf1002745b1b9e2148f15afbc7e01
Peter Eder <petereder89@gmail.com>
2014-11-11 18:50:32 +0100

    changed spring config and

--- src/main/java/rentaroom/guest/GuestController.java
+++ src/main/java/rentaroom/guest/GuestController.java
@@ -2,7 +2,6 @@ package rentaroom.guest;
  
 import javax.servlet.http.HttpServletRequest;
 
-import rentaroom.db.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,23 +13,13 @@ public class GuestController {
     @Autowired
     private GuestDao guestDao;
 
-    @Autowired
-    private CustomerRepository customerRepo;
-
-
     @RequestMapping(value= "/rentaroom/guest")
     public ModelAndView guestbook(HttpServletRequest request) {
         // Handle a new rentaroom.guest (if any):
         String name = request.getParameter("name");
 
-
-        customerRepo.findByName("dummy");
-
         if (name != null)
             guestDao.persist(new Guest(name));
-
-
-
 
         // Prepare the result view (rentaroom.guest.jsp):
         return new ModelAndView("guest.jsp", "guestDao", guestDao);

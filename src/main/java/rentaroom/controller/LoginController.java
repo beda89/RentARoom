6b02eb7ad551646cc349293cb1982384c900fb8c
Peter Eder <petereder89@gmail.com>
2014-11-28 16:15:33 +0100

    added test.jsp

--- src/main/java/rentaroom/controller/LoginController.java
+++ src/main/java/rentaroom/controller/LoginController.java
@@ -2,11 +2,14 @@ package rentaroom.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import rentaroom.db.repositories.CustomerRepository;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Peter on 11.11.2014.
@@ -18,18 +21,12 @@ public class LoginController {
     @Autowired
     private CustomerRepository customerRepo;
 
-    @RequestMapping(value= "/guest")
-    public ModelAndView guestbook(HttpServletRequest request) {
-        // Handle a new rentaroom.guest (if any):
-       /* String name = request.getParameter("name");
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello(ModelMap model) {
 
-        if (name != null)
-            guestDao.persist(new Guest(name));
+        model.addAttribute("name", "JCG Hello World!");
+        return "test";
 
-        // Prepare the result view (rentaroom.guest.jsp):
-        return new ModelAndView("guest.jsp", "guestDao", guestDao); */
-
-        return null;
     }
 
 }
---
+++ src/main/webapp/WEB-INF/test.jsp
@@ -0,0 +1,9 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<body>
+<h1>Spring 4.0.2 MVC web service</h1>
+
+<h3>Name : ${name}</h3>
+</body>
+</html>
+

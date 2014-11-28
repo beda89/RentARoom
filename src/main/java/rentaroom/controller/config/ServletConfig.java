8cae5389100619357334ac02ccd71dbaa45103a5
Christopher Simerle <simerle.c@gmail.com>
2014-11-28 17:52:14 +0100

    fixed project setup

---
+++ src/main/java/rentaroom/controller/config/ServletConfig.java
@@ -0,0 +1,33 @@
+package rentaroom.controller.config;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Simerle Christopher
+ * Date: 28/11/14
+ * Time: 17:24
+ * To change this template use File | Settings | File Templates.
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration //Marks this class as configuration
+//Specifies which package to scan
+@ComponentScan("rentaroom.controller")
+//Enables Spring's annotations
+@EnableWebMvc
+public class ServletConfig {
+
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setPrefix("/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+}

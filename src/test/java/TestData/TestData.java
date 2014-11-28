e0ed834238ed47629983345dc5d36091bffb3bb5
Peter Eder <petereder89@gmail.com>
2014-11-28 17:02:25 +0100

    added cleanup of database

--- src/test/java/TestData/TestData.java
+++ src/test/java/TestData/TestData.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import rentaroom.db.repositories.CustomerRepository;
+import rentaroom.db.repositories.InvoiceRepository;
+import rentaroom.db.repositories.ReservationRepository;
+import rentaroom.db.repositories.RoomRepository;
 
 /**
  * Created by Peter on 28.11.2014.
@@ -15,9 +18,25 @@ public class TestData {
     @Autowired
     private CustomerRepository customerRepo;
 
+    @Autowired
+    private RoomRepository roomRepo;
+
+    @Autowired
+    private ReservationRepository reservationRepo;
+
+    @Autowired
+    private InvoiceRepository invoiceRepo;
+
     @Test
     public void initDBWithTestData(){
+        //setBackDatabase
         customerRepo.deleteAll();
+        roomRepo.deleteAll();
+        reservationRepo.deleteAll();
+        invoiceRepo.deleteAll();
+
+
+        //TODO: generate new testdata
 
 
     }

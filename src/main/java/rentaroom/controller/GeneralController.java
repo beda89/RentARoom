bb30378fdcc7e8d7e1a96220c0dca0ab3c1ef3e7
Christian Detamble <christian.detamble@hotmail.com>
2014-12-03 16:54:23 +0100

    Logged-in users will now be redirected to Rooms View instead of Login.

--- src/main/java/rentaroom/controller/GeneralController.java
+++ src/main/java/rentaroom/controller/GeneralController.java
@@ -1,10 +1,13 @@
 package rentaroom.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import rentaroom.entities.Customer;
+
+import java.security.Principal;
 
 /**
  * Created by Peter on 28.11.2014.
@@ -15,6 +18,11 @@ public class GeneralController {
     @RequestMapping(value = {"/about"}, method = RequestMethod.GET)
     public ModelAndView aboutPage() {
         return new ModelAndView("about");
+    }
+
+    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+    public ModelAndView indexPage(ModelMap model, Principal principal ) throws Exception {
+        return new ModelAndView(principal == null ? "login" : "rooms");
     }
 
 }
